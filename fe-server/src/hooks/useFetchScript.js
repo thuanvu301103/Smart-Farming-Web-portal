7aa3b142d91452f40531356390d96508816af857
cuson311 <cuson108@gmail.com>
2025-03-07 00:33:05 +0700

    FE Server: Add comment api and hook

--- fe-server/src/hooks/useFetchScript.js
+++ fe-server/src/hooks/useFetchScript.js
@@ -1,66 +1,63 @@
-﻿import { useState, useEffect, useCallback  } from "react";
+﻿import { useState, useEffect, useCallback } from "react";
 import scriptApi from "./../api/scriptAPI";
 
 const useFetchScriptInfo = (userId, scriptId) => {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    //console.log("Calling fetch Data:", { userId, scriptId });
-    useEffect(() => {
-        //console.log("UseEffect is running");
-        const fetchData = async () => {
-            setLoading(true);
-            setError(null);
-            try {
-                const data = await scriptApi.getScriptInfo(userId, scriptId);
-                //console.log("Get script Info: ", data);
-                setData(data);
-            } catch (err) {
-                console.error("Error fetching script info:", err);
-                setError(err);
-            } finally {
-                setLoading(false);
-            }
-        };
-        fetchData();
-    }, [userId, scriptId]);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  //   console.log("Calling fetch Data:", { userId, scriptId });
+  useEffect(() => {
+    console.log("UseEffect is running");
+    const fetchData = async () => {
+      setLoading(true);
+      setError(null);
+      try {
+        const data = await scriptApi.getScriptInfo(userId, scriptId);
+        // console.log("Get script Info: ", data);
+        setData(data);
+      } catch (err) {
+        console.error("Error fetching script info:", err);
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [userId, scriptId]);
 
-    return { data, setData, loading, error };
+  return { data, setData, loading, error };
 };
 
 const useFetchScriptFile = (filePath) => {
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    const fetchData = useCallback(async () => {
-        setLoading(true);
-        setError(null);
-        try {
-            const data = await scriptApi.getScriptFile(filePath);
-            //console.log("Fetching File data: ", data);
-            setData(JSON.stringify(data, null, 2));
-        } catch (err) {
-            console.error("Error fetching script file:", err);
-            setError(err);
-        } finally {
-            setLoading(false);
-        }
-    }, [filePath]);
+  const fetchData = useCallback(async () => {
+    setLoading(true);
+    setError(null);
+    try {
+      const data = await scriptApi.getScriptFile(filePath);
+      //console.log("Fetching File data: ", data);
+      setData(JSON.stringify(data, null, 2));
+    } catch (err) {
+      console.error("Error fetching script file:", err);
+      setError(err);
+    } finally {
+      setLoading(false);
+    }
+  }, [filePath]);
 
-    useEffect(() => {
-        fetchData();
-    }, [filePath]);
+  useEffect(() => {
+    fetchData();
+  }, [filePath]);
 
-    // Reload function
-    const reload = () => {
-        fetchData();
-    };
+  // Reload function
+  const reload = () => {
+    fetchData();
+  };
 
-    return { data, setData, loading, error, reload };
+  return { data, setData, loading, error, reload };
 };
 
-export {
-    useFetchScriptInfo,
-    useFetchScriptFile
-};
+export { useFetchScriptInfo, useFetchScriptFile };
--- fe-server/src/pages/ScriptComment.js
+++ fe-server/src/pages/ScriptComment.js
@@ -1,217 +1,292 @@
 import React, { useEffect, useState } from "react";
-import { Grid, Box, List, ListItem, Modal, Typography, Stack, Button, Snackbar, Alert, TextField, IconButton, Avatar } from "@mui/material";
+import {
+  Grid,
+  Box,
+  List,
+  ListItem,
+  Modal,
+  Typography,
+  Stack,
+  Button,
+  Snackbar,
+  Alert,
+  TextField,
+  IconButton,
+  Avatar,
+} from "@mui/material";
 import { useTranslation } from "react-i18next";
 import axios from "axios";
+import { useParams } from "react-router-dom";
 import ScriptCommentItem from "../components/ScriptCommentItem";
 //Import icon
 import SendIcon from "@mui/icons-material/Send";
 import PersonIcon from "@mui/icons-material/Person";
-const ScriptComment = ({ scriptInfo }) => {
-    const { t } = useTranslation();
-    const [allComments, setAllComments] = useState([]);
-    const [usernames, setUsernames] = useState({});
-    const [deleteModalOpen, setDeleteModalOpen] = useState(false);
-    const [commentToDelete, setCommentToDelete] = useState(null);
-    const [snackbarOpen, setSnackbarOpen] = useState(false);  // Snackbar state
-    const [snackbarMessage, setSnackbarMessage] = useState('');  // Snackbar message
-    const [newComment, setNewComment] = useState(''); // State for new comment input
+import { useFetchScriptInfo } from "../hooks/useFetchScript";
+import {
+  useFetchComments,
+  useFetchSubComments,
+  useFetchCommentHistory,
+} from "../hooks/useFetchComment";
+import userApi from "../api/userAPI";
+import commentApi from "../api/commentAPI";
+const ScriptComment = () => {
+  const { t } = useTranslation();
+  const { userId, scriptId } = useParams();
+  //   const [allComments, setAllComments] = useState([]);
+  const [usernames, setUsernames] = useState({});
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+  const [commentToDelete, setCommentToDelete] = useState(null);
+  const [snackbarOpen, setSnackbarOpen] = useState(false); // Snackbar state
+  const [snackbarMessage, setSnackbarMessage] = useState(""); // Snackbar message
+  const [newComment, setNewComment] = useState(""); // State for new comment input
 
-    const getAllComments = async () => {
+  const {
+    data: scriptInfo,
+    setData: setScriptInfo,
+    loading: scriptInfoLoading,
+    error: scriptInfoError,
+  } = useFetchScriptInfo(userId, scriptId);
+
+  const { data: allComments, setData: setAllComments } = useFetchComments(
+    userId,
+    scriptId
+  );
+
+  const getAllComments = async () => {
+    try {
+      const response = await commentApi.getAllComments(userId, scriptId);
+      setAllComments(response);
+    } catch (error) {
+      console.error("Error getting comments:", error);
+    }
+  };
+
+  //   useEffect(() => {
+  //     if (!scriptInfo?.owner_id || !scriptInfo?._id) return;
+  //     getAllComments();
+  //   }, [scriptInfo?.owner_id, scriptInfo?._id]);
+
+  useEffect(() => {
+    const fetchUsernames = async () => {
+      const newUsernames = { ...usernames };
+      for (const comment of allComments) {
         try {
-            const response = await axios.get(`http://localhost:3000/${scriptInfo.owner_id}/scripts/${scriptInfo._id}/comments`);
-            setAllComments(response.data);
+          const user = await userApi.profile(comment.owner_id);
+          newUsernames[comment.owner_id] = user.username;
         } catch (error) {
-            console.error("Error getting comments:", error);
+          console.error("Error getting username:", error);
         }
-    };
-    
-    useEffect(() => {
-        if (!scriptInfo.owner_id || !scriptInfo._id) return;
-        getAllComments();
-    }, [scriptInfo.owner_id, scriptInfo._id]);
-    
+      }
 
-    useEffect(() => {
-        const fetchUsernames = async () => {
-            const newUsernames = { ...usernames };
-            for (const comment of allComments) {
-                if (!newUsernames[comment.owner_id]) {
-                    try {
-                        const response = await axios.get(`http://localhost:3000/${comment.owner_id}/profile`);
-                        newUsernames[comment.owner_id] = response.data.username;
-                    } catch (error) {
-                        console.error("Error getting username:", error);
-                        newUsernames[comment.owner_id] = "Unknown";
-                    }
-                }
-            }
-            setUsernames(newUsernames);
-        };
-
-        if (allComments.length > 0) fetchUsernames();
-    }, [allComments]);
-
-    const handleEdit = async (comment, content) => {
-        if (!content.trim()) return;
-        try {
-            const response = await axios.put(`http://localhost:3000/${comment.owner_id}/scripts/${scriptInfo._id}/comments/${comment._id}`,  { content });
-            console.log("Update comment", response.data);
-
-            await getAllComments();
-
-            // Show Snackbar on success
-            setSnackbarMessage(t("edit-comment.success"));
-            setSnackbarOpen(true);
-        } catch (error) {
-            console.error("Error editing comment:", error);
-            setSnackbarMessage(t("edit-comment.error"));
-            setSnackbarOpen(true);
-        }
+      setUsernames(newUsernames);
     };
 
-    const handleReply = (comment) => {
-        console.log("Replying to:", comment);
+    if (allComments?.length > 0) fetchUsernames();
+  }, [allComments]);
+
+  const handleEdit = async (comment, content) => {
+    if (!content.trim()) return;
+    try {
+      const response = await commentApi.updateComment(
+        userId,
+        scriptId,
+        comment._id,
+        content
+      );
+      console.log("Update comment", response, content);
+
+      await getAllComments();
+
+      // Show Snackbar on success
+      setSnackbarMessage(t("edit-comment.success"));
+      setSnackbarOpen(true);
+    } catch (error) {
+      console.error("Error editing comment:", error);
+      setSnackbarMessage(t("edit-comment.error"));
+      setSnackbarOpen(true);
+    }
+  };
+
+  const handleReply = async (comment, replyContent) => {
+    if (!replyContent.trim()) return;
+
+    const replyCommentData = {
+      content: replyContent,
+      ownerId: localStorage.getItem("userId"),
+      subCommentId: comment._id,
+      // owner_id: scriptInfo.owner_id,
     };
 
-    const confirmDelete = (comment) => {
-        setCommentToDelete(comment);
-        setDeleteModalOpen(true);
-    };
+    try {
+      const response = await commentApi.createComment(
+        userId,
+        scriptId,
+        replyCommentData
+      );
+      console.log(response);
 
-    const handleDelete = async () => {
-        if (!commentToDelete) return;
-        try {
-            await axios.delete(`http://localhost:3000/${commentToDelete.owner_id}/scripts/${scriptInfo._id}/comments/${commentToDelete._id}`);
-            setAllComments(allComments.filter(comments => comments._id !== commentToDelete._id));
-            setDeleteModalOpen(false);
-            setCommentToDelete(null);
+      await getAllComments();
+    } catch (error) {
+      console.error("Error sending reply:", error);
+    }
+  };
 
-            // Show Snackbar on success
-            setSnackbarMessage(t("delete-comment.success"));
-            setSnackbarOpen(true); // Open Snackbar
+  const confirmDelete = (comment) => {
+    setCommentToDelete(comment);
+    setDeleteModalOpen(true);
+  };
 
-        } catch (error) {
-            console.error("Error deleting comment:", error);
-        }
-    };
+  const handleDelete = async () => {
+    if (!commentToDelete) return;
+    try {
+      await commentApi.deleteComment(userId, scriptId, commentToDelete._id);
+      await getAllComments();
 
-    const handleAddComment = async () => {
-        if (!newComment.trim()) return;
+      setDeleteModalOpen(false);
+      setCommentToDelete(null);
 
-        try {
-            const newCommentData = {
-                content: newComment,
-                ownerId: '679b765e8496f00b99063cb8',
-                // owner_id: scriptInfo.owner_id,
-            };
-            const response = await axios.post(`http://localhost:3000/${scriptInfo.owner_id}/scripts/${scriptInfo._id}/comments`, newCommentData);
-            console.log("New comment", response.data);
-            setNewComment(''); 
+      // Show Snackbar on success
+      setSnackbarMessage(t("delete-comment.success"));
+      setSnackbarOpen(true); // Open Snackbar
+    } catch (error) {
+      console.error("Error deleting comment:", error);
+    }
+  };
 
-            await getAllComments();
+  const handleAddComment = async () => {
+    if (!newComment.trim()) return;
 
-            // Show Snackbar on success
-            setSnackbarMessage(t("add-comment.success"));
-            setSnackbarOpen(true);
-        } catch (error) {
-            console.error("Error adding comment:", error);
-            setSnackbarMessage(t("add-comment.error"));
-            setSnackbarOpen(true);
-        }
-    };
+    try {
+      const newCommentData = {
+        content: newComment,
+        ownerId: localStorage.getItem("userId"),
+      };
+      const response = await commentApi.createComment(
+        userId,
+        scriptId,
+        newCommentData
+      );
+      console.log("New comment", response);
+      setNewComment("");
 
-    const handleCloseSnackbar = () => {
-        setSnackbarOpen(false); // Close Snackbar
-    };
+      await getAllComments();
 
-    return (
-        <Box className="main-content" sx={{ width: "100%", maxWidth: '80vw', mx: "auto", p: 2 }}>
-            <Grid container alignItems="start">
-                <List sx={{ width: "100%" }}>
-                    {allComments.map((comment, index) => (
-                        <ListItem key={index} sx={{ p: 0 }}>
-                            <ScriptCommentItem
-                                comment={comment}
-                                usernames={usernames}
-                                handleReply={handleReply}
-                                handleEdit={handleEdit}
-                                confirmDelete={confirmDelete}
-                                scriptInfo={scriptInfo}
-                            />
-                        </ListItem>
-                    ))}
-                </List>
-            </Grid>
+      // Show Snackbar on success
+      setSnackbarMessage(t("add-comment.success"));
+      setSnackbarOpen(true);
+    } catch (error) {
+      console.error("Error adding comment:", error);
+      setSnackbarMessage(t("add-comment.error"));
+      setSnackbarOpen(true);
+    }
+  };
 
-            {/* Add Comment Section */}
-            <Box sx={{ mt: 3 }}>
-                <Stack direction="row" spacing={2} alignItems="center">
-                    <Avatar sx={{ bgcolor: "#1976d2", fontSize: 16 }}>
-                        {<PersonIcon />}
-                    </Avatar>
-                    <TextField
-                        label={t("add-comment.placeholder")}
-                        variant="outlined"
-                        value={newComment}
-                        onChange={(e) => setNewComment(e.target.value)}
-                        multiline
-                        minRows={1}
-                        maxRows={6}
-                        sx={{ width: "100%" }}
-                    />
-                    <IconButton
-                        color="primary"
-                        onClick={handleAddComment}
-                        sx={{ mt: 2 }}
-                    >
-                        <SendIcon />
-                    </IconButton>
-                </Stack>
-            </Box>
+  const handleCloseSnackbar = () => {
+    setSnackbarOpen(false); // Close Snackbar
+  };
 
-            {/* Delete Confirmation Modal */}
-            <Modal open={deleteModalOpen} onClose={() => setDeleteModalOpen(false)}>
-                <Box sx={{
-                    position: "absolute",
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%, -50%)",
-                    width: 600,
-                    bgcolor: "background.paper",
-                    boxShadow: 24,
-                    p: 3,
-                    borderRadius: 2,
-                    textAlign: "center"
-                }}>
-                    <Typography variant="h6" gutterBottom>
-                        {t("delete-comment.title")}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary" gutterBottom>
-                        {t("delete-comment.content")}
-                    </Typography>
-                    <Stack direction="row" spacing={2} justifyContent="center" sx={{ mt: 2 }}>
-                        <Button variant="outlined" onClick={() => setDeleteModalOpen(false)}>
-                            {t("button.cancel")}
-                        </Button>
-                        <Button variant="contained" color="error" onClick={handleDelete}>
-                            {t("button.delete")}
-                        </Button>
-                    </Stack>
-                </Box>
-            </Modal>
+  return (
+    <Box
+      className="main-content"
+      sx={{ width: "100%", maxWidth: "80vw", mx: "auto", p: 2 }}
+    >
+      <Grid container alignItems="start">
+        <List sx={{ width: "100%" }}>
+          {allComments?.map((comment, index) => (
+            <ListItem key={index} sx={{ p: 0 }}>
+              <ScriptCommentItem
+                comment={comment}
+                usernames={usernames}
+                handleReply={handleReply}
+                handleEdit={handleEdit}
+                confirmDelete={confirmDelete}
+                scriptInfo={scriptInfo}
+                isSubComment={false}
+              />
+            </ListItem>
+          ))}
+        </List>
+      </Grid>
 
-            {/* Snackbar for Add/Delete Success */}
-            <Snackbar
-                open={snackbarOpen}
-                autoHideDuration={3000}
-                onClose={handleCloseSnackbar}
+      {/* Add Comment Section */}
+      <Box sx={{ mt: 3 }}>
+        <Stack direction="row" spacing={2} alignItems="center">
+          <Avatar sx={{ bgcolor: "#1976d2", fontSize: 16 }}>
+            {<PersonIcon />}
+          </Avatar>
+          <TextField
+            label={t("add-comment.placeholder")}
+            variant="outlined"
+            value={newComment}
+            onChange={(e) => setNewComment(e.target.value)}
+            multiline
+            minRows={1}
+            maxRows={6}
+            sx={{ width: "100%" }}
+          />
+          <IconButton color="primary" onClick={handleAddComment} sx={{ mt: 2 }}>
+            <SendIcon />
+          </IconButton>
+        </Stack>
+      </Box>
+
+      {/* Delete Confirmation Modal */}
+      <Modal open={deleteModalOpen} onClose={() => setDeleteModalOpen(false)}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 600,
+            bgcolor: "background.paper",
+            boxShadow: 24,
+            p: 3,
+            borderRadius: 2,
+            textAlign: "center",
+          }}
+        >
+          <Typography variant="h6" gutterBottom>
+            {t("delete-comment.title")}
+          </Typography>
+          <Typography variant="body2" color="text.secondary" gutterBottom>
+            {t("delete-comment.content")}
+          </Typography>
+          <Stack
+            direction="row"
+            spacing={2}
+            justifyContent="center"
+            sx={{ mt: 2 }}
+          >
+            <Button
+              variant="outlined"
+              onClick={() => setDeleteModalOpen(false)}
             >
-                <Alert onClose={handleCloseSnackbar} severity="success" sx={{ width: '100%' }}>
-                    {snackbarMessage}
-                </Alert>
-            </Snackbar>
+              {t("button.cancel")}
+            </Button>
+            <Button variant="contained" color="error" onClick={handleDelete}>
+              {t("button.delete")}
+            </Button>
+          </Stack>
         </Box>
-    );
+      </Modal>
+
+      {/* Snackbar for Add/Delete Success */}
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={3000}
+        onClose={handleCloseSnackbar}
+      >
+        <Alert
+          onClose={handleCloseSnackbar}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
+    </Box>
+  );
 };
 
 export default ScriptComment;
