9d595c0390886f6c2c7ee4a72f7ce7f6331224d3
thuanvu301103 <96073973+thuanvu301103@users.noreply.github.com>
2025-03-17 15:15:43 +0700

    BE: Build Get Activity feature

--- be-server/src/modules/activities/activities.module.ts
+++ be-server/src/modules/activities/activities.module.ts
@@ -7,12 +7,20 @@ import { ScriptsModule } from "../scripts/scripts.module";
 import { ModelsModule } from "../models/models.module";
 import { CommentsModule } from "../comments/comments.module";
 import { Activity, ActivitySchema } from '../../schemas/activities.schema';
+import { Script, ScriptSchema } from '../../schemas/scripts.schema';
+import { Models, ModelSchema } from '../../schemas/models.schema';
+import { Comment, CommentSchema } from '../../schemas/comments.schema';
 
 @Module({
     imports: [
         forwardRef(() => ModelsModule),
         forwardRef(() => CommentsModule),
-        MongooseModule.forFeature([{ name: Activity.name, schema: ActivitySchema }]),
+        MongooseModule.forFeature([
+            { name: Activity.name, schema: ActivitySchema },
+            { name: Script.name, schema: ScriptSchema },
+            { name: Models.name, schema: ModelSchema },
+            { name: Comment.name, schema: CommentSchema },
+        ]),
         forwardRef(() => ScriptsModule),
     ],
     controllers: [ActivitiesController],
