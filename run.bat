8dea5247bc3524e2d3e2ee522344fe543505590c
thuanvu301103 <96073973+thuanvu301103@users.noreply.github.com>
2025-03-24 11:20:54 +0700

    Rewrite run.bat for starting MLflow Trackinh Server

--- run.bat
+++ run.bat
@@ -5,7 +5,8 @@ echo Select an option:
 echo [1] Run both backend and frontend
 echo [2] Run backend only
 echo [3] Run frontend only
-set /p choice=Enter your choice (1, 2, 3): 
+echo [4] Run MLflow Tracking Server
+set /p choice=Enter your choice (1, 2, 3. 4): 
 
 if "!choice!"=="1" (
     echo Starting backend and frontend...
@@ -17,6 +18,9 @@ if "!choice!"=="1" (
 ) else if "!choice!"=="3" (
     echo Starting frontend...
     start cmd /k "cd fe-server && npm start"
+) else if "!choice!"=="4" (
+    echo Starting MLflow Tracking Server...
+    start cmd /k "mlflow server --backend-store-uri sqlite:///mlflow.db --default-artifact-root ./mlruns --host 0.0.0.0 --port 5000"
 ) else (
     echo Invalid choice. Exiting.
 )
