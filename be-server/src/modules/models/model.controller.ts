6bbe5d9f29dacbd24a4978045e248e316d738a49
thuanvu301103 <96073973+thuanvu301103@users.noreply.github.com>
2025-05-16 20:00:20 +0700

    Update model.controller.ts
    Add Update Model Controller

--- be-server/src/modules/models/model.controller.ts
+++ be-server/src/modules/models/model.controller.ts
@@ -31,4 +31,24 @@ export class ModelController{
     ) {
         return await this.modelsService.getModel(name);
     }
+
+    @Post('rename')
+    async createModel(
+        @Body() data: {
+            name: string,
+            new_name: string
+        }
+    ) {
+        return await this.modelsService.renameModel(data.name, data.new_name);
+    }
+
+    @Post('update')
+    async updateModel(
+        @Body() data: {
+            name: string,
+            description: string
+        }
+    ) {
+        return await this.modelsService.updateModel(data.name, data.description);
+    }
 }
