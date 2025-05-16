940fe39007b197209b57a31b92e599318265700e
thuanvu301103 <96073973+thuanvu301103@users.noreply.github.com>
2025-05-16 17:22:29 +0700

    Update model.controller.ts

--- be-server/src/modules/models/model.controller.ts
+++ be-server/src/modules/models/model.controller.ts
@@ -21,7 +21,7 @@ export class ModelController{
             description: string
         }
     ) {
-        console.log("Creating new Model: ", name, '-', description);
+        console.log("Creating new Model: ", data.name, '-', data.description);
         return await this.modelsService.createModel(data.name, data.tags, data.description);
     }
 }
