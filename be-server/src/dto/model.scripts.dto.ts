93af7f79ace464c1206e35a4b4ce519330f098c1
thuanvu301103 <96073973+thuanvu301103@users.noreply.github.com>
2025-05-18 11:46:23 +0700

    Create model.scripts.dto.ts

---
+++ be-server/src/dto/model.scripts.dto.ts
@@ -0,0 +1,48 @@
+import {
+    IsOptional,
+    IsString,
+    IsEnum,
+    IsNumberString, IsInt, Min,
+    IsArray,
+    IsMongoId,
+    IsNotEmpty,
+    IsIn,
+} from 'class-validator';
+import { Transform, Type } from 'class-transformer';
+
+enum SortOrder {
+    ASC = 'asc',
+    DESC = 'desc',
+}
+
+class BaseSearchModelScriptQueryDto {
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(1)
+    page?: number = 1;
+
+    @IsOptional()
+    @Type(() => Number)
+    @IsInt()
+    @Min(1)
+    limit?: number = 10;
+
+    @IsOptional()
+    @IsString()
+    sortBy?: string = 'createdAt';
+
+    @IsOptional()
+    @IsEnum(SortOrder)
+    order?: SortOrder = SortOrder.DESC;
+
+    @IsOptional()
+    @IsString()
+    locations?: string;
+
+    @IsOptional()
+    @IsString()
+    model_name?: string;
+}
+
+export { BaseSearchModelScriptQueryDto }
